7a6e6eb79889492af50513ac7f0678051f631cca
Long Le <cuncongame@gmail.com>
2021-07-26 12:09:30 +0700

    Update capture.py

--- server/src/capture.py
+++ server/src/capture.py
@@ -4,13 +4,7 @@ from flask import Blueprint, Response
 from firebase import Firebase
 
 config = {
-    "apiKey": "AIzaSyCfTKQ-5sDNvN3QfC6S4oqeKOnbEv7AxzE",
-    "authDomain": "iot-smart-home-door-lock-7ccc9.firebaseapp.com",
-    "databaseURL": "https://iot-smart-home-door-lock-7ccc9.firebaseio.com",
-    "projectId": "iot-smart-home-door-lock-7ccc9",
-    "storageBucket": "iot-smart-home-door-lock-7ccc9.appspot.com",
-    "messagingSenderId": "513607106040",
-    "appId": "1:513607106040:web:d627644390357454e76a0d"
+    
 }
 
 firebase = Firebase(config)
