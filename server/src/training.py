d25782a96cb0bc11ed88f917c56e61fce69b31c5
Long Le <cuncongame@gmail.com>
2021-07-26 12:10:03 +0700

    Update training.py

--- server/src/training.py
+++ server/src/training.py
@@ -6,13 +6,7 @@ from PIL import Image
 from firebase import Firebase
 
 config = {
-    "apiKey": "AIzaSyCfTKQ-5sDNvN3QfC6S4oqeKOnbEv7AxzE",
-    "authDomain": "iot-smart-home-door-lock-7ccc9.firebaseapp.com",
-    "databaseURL": "https://iot-smart-home-door-lock-7ccc9.firebaseio.com",
-    "projectId": "iot-smart-home-door-lock-7ccc9",
-    "storageBucket": "iot-smart-home-door-lock-7ccc9.appspot.com",
-    "messagingSenderId": "513607106040",
-    "appId": "1:513607106040:web:d627644390357454e76a0d"
+    
 }
 
 bp_training = Blueprint('training', __name__)
